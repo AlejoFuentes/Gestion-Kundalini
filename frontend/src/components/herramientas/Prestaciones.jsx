ba7fa42be9cfe7139091a773c45479d0fdccc16e
AlejoFuentes <alejo.fuentess04@gmail.com>
2026-06-11 03:48:34 -0300

    agrego botones sin funcionalidad a las prestaciones

--- frontend/src/components/herramientas/Prestaciones.jsx
+++ frontend/src/components/herramientas/Prestaciones.jsx
@@ -16,6 +16,44 @@ const Prestaciones = () => {
                 setPrestaciones(res);
             })
         },[]);
+    
+    const accionesBotones = [
+
+        { nombre: "Editar Prestación", 
+          icono: "bi bi-pencil-square",
+          color: "rgb(165, 22, 22)",
+        //   accion: (f) => {
+        //     setFacturaActual(f); 
+        //     setEstadoSeleccionado(f.estado);
+        //     setNumeroSeleccionado(f.numero || "");
+        //     setMesSeleccionado(f.mes ? `${String(f.mes).slice(0, 4)}-${String(f.mes).slice(4)}` : "");
+        //     setFechaPagoSeleccionada(f.fecha_pago);
+        //   },
+        //   modalTarget: "#modalEditarFactura"
+        },
+
+        { nombre: "Duplicar Prestación", 
+          icono: "bi bi-back",
+          color: "rgb(8, 6, 6)",
+        //   accion: (f) => {
+        //     setFacturaActual(f);
+        //     setNumeroSeleccionado("2");
+        //     setMesSeleccionado(`${String(f.mes).slice(0, 4)}-${String(f.mes).slice(4)}`)
+        //   },
+        //   mostrar: (f) => !f.numero,
+        //   modalTarget: "#modalDividirFactura"
+         },
+
+        { nombre: "Eliminar Prestación",
+          icono: "bi bi-trash3-fill",
+          color: "rgb(165, 22, 22)",
+        //   accion: (f) => {
+        //     setFacturaActual(f);
+        //   },  
+        //   mostrar: (f) => f.abierto === "SI",
+        //   modalTarget: "#modalEliminarFactura",
+        },
+    ];
 
     const prestadoresUnicos = ['Todos', ...new Set(prestaciones?.map(p => p.prestador))];
 
@@ -93,7 +131,7 @@ const Prestaciones = () => {
             </div>
 
             {/* <-- LISTA --> */}
-            <ul className="list-group gap-3">
+            <ul className="list-group gap-3 mb-5">
                 {prestacionesFiltradas && prestacionesFiltradas.length > 0 ? (
             prestacionesFiltradas.map(p => (
                 <li className='list-group-item d-flex row mx-4 p-4 rounded-3' key={p.id}>
@@ -104,11 +142,17 @@ const Prestaciones = () => {
                         <strong className='d-block'>Fecha Inicio: <span className="fw-normal">{formatearFecha(p.fecha_inicio)}</span></strong>
                         <strong className='d-block'>Fecha Fin: <span className="fw-normal">{p.fecha_fin? formatearFecha(p.fecha_fin) : "- -"}</span></strong>
                         <strong className='d-block'>Especialidad: <span className="fw-normal">{p.especialidad}</span></strong>
+                        {p.detalles_extras?.turno && (
+                        <strong className='d-block'>Turno: <span className="fw-normal">{p.detalles_extras.turno}</span></strong>
+                        )}
+                        {p.detalles_extras?.centro_medico && (
+                        <strong className='d-block'>Centro: <span className="fw-normal">{p.detalles_extras.centro_medico}</span></strong>
+                        )}
                     </div>
                     {/* <-- COLUMNA 2 --> */}
-                    <div className='col-4 border-end'>
+                    <div className='col-3 border-end'>
                         <strong className='d-block mb-2'>Paciente: <span className="fw-normal">{p.paciente.nombre} {p.paciente.apellido}</span></strong>
-                        <strong className='d-block'>Fecha de nacimiento: <span className="fw-normal">{formatearFecha(p.paciente.fecha_nacimiento)}</span></strong>
+                        <strong className='d-block'>Fecha Nac: <span className="fw-normal">{formatearFecha(p.paciente.fecha_nacimiento)}</span></strong>
                         <strong className='d-block'>Diagnóstico: <span className="fw-normal">{p.paciente.diagnostico}</span></strong>
                         <strong className='d-block'>Dirección: <span className="fw-normal">{p.paciente.direccion}</span></strong>
                         <strong className='d-block'>Localidad: <span className="fw-normal">{p.paciente.localidad}</span></strong>
@@ -122,8 +166,54 @@ const Prestaciones = () => {
                         <strong className='d-block'>Estado: <span className="fw-normal">{p.pagado ? "Pagada" : "Impaga"}</span></strong>
                     </div>
                     {/* <-- COLUMNA 4 --> */}
-                    <div className='col-4 d-flex align-items-center'>
-                        <strong className='d-block'>Observaciones: <span className="fw-normal">{p.observaciones}</span></strong>
+                    <div className='row col-4 d-flex border-end'>
+                        <div className='col-6'>
+                            {p.detalles_extras?.hs_doble && p.detalles_extras?.valor_hs_doble && (
+                                <>
+                                    <strong className='d-block border-end'>Cant hs dobles: <span className="fw-normal">{p.detalles_extras.hs_doble}</span></strong>
+                                    <strong className='d-block border-end'>Valor hs dobles: <span className="fw-normal">${formatearMoneda(p.detalles_extras.valor_hs_doble)}</span></strong>
+                                </>
+                            )}
+                            {p.detalles_extras?.hs_extra && p.detalles_extras?.valor_hs_extra && (
+                                <>
+                                    <strong className='d-block border-end'>Cant hs extra: <span className="fw-normal">{p.detalles_extras.hs_extra}</span></strong>
+                                    <strong className='d-block border-end'>Valor hs extra: <span className="fw-normal">${formatearMoneda(p.detalles_extras.valor_hs_extra)}</span></strong>
+                                </>
+                            )}
+
+                        </div>
+                        {/* <-- COLUMNA 5 --> */}
+                        <div className='col-6 mb-2'>
+                            {p.detalles_extras?.hs_feriado && p.detalles_extras?.valor_hs_feriado && (
+                                <>
+                                    <strong className='d-block'>Cant hs feriado: <span className="fw-normal">{p.detalles_extras.hs_feriado}</span></strong>
+                                    <strong className='d-block'>Valor hs feriado: <span className="fw-normal">${formatearMoneda(p.detalles_extras.valor_hs_feriado)}</span></strong>
+                                </>
+                            )}
+                            {p.detalles_extras?.hs_e_f && p.detalles_extras?.valor_hs_e_f && (
+                                <>
+                                    <strong className='d-block'>Cant hs e+f: <span className="fw-normal">{p.detalles_extras.hs_e_f}</span></strong>
+                                    <strong className='d-block'>Valor hs e+f: <span className="fw-normal">${formatearMoneda(p.detalles_extras.valor_hs_e_f)}</span></strong>
+                                </>
+                            )}
+                        </div>
+                        <strong className='d-block col-12'>Observaciones: <span className="fw-normal">{p.observaciones}</span></strong>
+                    </div>
+                    {/* <-- COLUMNA 5 --> */}
+                    <div className='col-1 d-flex flex-column align-items-end'>
+                        {accionesBotones.map((boton, index) => (
+                            <button 
+                                key={index}
+                                className='btn' 
+                                type='button'
+                                title={boton.nombre}
+                                onClick={() => boton.accion(factura)}
+                                data-bs-toggle={boton.modalTarget ? "modal" : undefined}
+                                data-bs-target={boton.modalTarget}
+                            >
+                                <i className={`${boton.icono} fs-4 m-0 botones-accion`} style={{color: `${boton.color}`}}></i>   
+                            </button>
+                        ))}
                     </div>
                 </li>
                 ))) : (
